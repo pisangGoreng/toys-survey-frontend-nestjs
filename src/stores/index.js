1092cf70a276869c0f8840addd2b6f05d9c7bd4d
endy <7ofpentacles@gmail.com>
2022-10-13 23:20:06 +0700

    add persist

--- src/stores/index.js
+++ src/stores/index.js
@@ -1,6 +1,11 @@
 import { createStore } from "vuex"
+import VuexPersistence from "vuex-persist"
 
 import store from "./modules/store"
+
+const vuexLocal = new VuexPersistence({
+  storage: window.localStorage,
+})
 
 export default createStore({
   modules: {
@@ -9,5 +14,5 @@ export default createStore({
   state: () => {},
   actions: {},
   mutations: {},
-  plugins: [],
+  plugins: [vuexLocal.plugin],
 })
