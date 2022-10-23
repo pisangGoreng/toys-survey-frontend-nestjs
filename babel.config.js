df6feb267d6dc9608e2416e2150471d3e7c32d08
endy <7ofpentacles@gmail.com>
2022-10-23 23:22:11 +0700

    test 5

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,9 @@
-// module.exports = {
-//   presets: ["@vue/cli-plugin-babel/preset"],
-// };
+module.exports = {
+  presets: ["@vue/cli-plugin-babel/preset"],
+}
 
 // module.exports = {
 //   presets: ["@vue/app"],
 // }
 
-module.exports = { presets: [["@vue/app", { useBuiltIns: "entry" }]] }
+// module.exports = { presets: [["@vue/app", { useBuiltIns: "entry" }]] }
